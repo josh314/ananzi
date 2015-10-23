279acee5fd9a616c1e6dbafc4e52f0980cd21fd3
Joshua Davis <josh314@gmail.com>
2015-10-23 16:42:32 -0400

    Added test-script with asynchronous fileio.

--- test-save.py
+++ test-save.py
@@ -2,30 +2,23 @@ import os.path
 from urllib.parse import urlparse
 import asyncio
 
-import aiofiles
-
 from ananzi.crawler import Crawler
 
-class AiofilesScraper(object):
-    def __init__(self,loop,save_dir='.'):
-        self.loop = loop
+class SaveScraper(object):
+    def __init__(self,save_dir='.'):
         self.save_dir = save_dir
 
-    @asyncio.coroutine
     def save(self,url,html):
         res = urlparse(url)
         filename = res.netloc + ".html"
         path = os.path.join(self.save_dir, filename)
-        f = yield from aiofiles.open(path,'wb')
-        try:
-            yield from f.write(html)
-            print("Saved: " + url)
-        finally:
-            yield from f.close()
+        f = open(path,'wb')
+        f.write(html)
+        f.close()
          
     def process(self, url, html):
         print("Processing: " + url)
-        asyncio.Task(self.save(url, html))
+        self.save(url,html)
         return (True, ['http://cnn.com', 'http://www.hockeybuzz.com'])
         
 urls = [
@@ -37,7 +30,7 @@ urls = [
 ]
 
 loop = asyncio.get_event_loop()
-cr = Crawler(loop, AiofilesScraper(loop,save_dir='tmp'))
+cr = Crawler(loop, SaveScraper(save_dir='tmp'))
 cr.launch(urls)
 print("Successful: {}".format(len(cr.done)))
 print("Failed: {}".format(len(cr.failed)))
